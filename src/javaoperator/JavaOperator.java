b352594701773310f6bebf031a9de57b00dda1ea
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2019-08-22 13:56:01 +0700

    first commit

---
+++ src/javaoperator/JavaOperator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoperator;
+
+/**
+ *
+ * @author yusri
+ */
+public class JavaOperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a=10;
+       int b=12;
+       int c;
+       //Penjumlahan
+       c=a+b;
+       System.out.println(a+"+"+b+"="+c);
+       //Perkalian
+       c=a*b;
+       System.out.println(a+"x"+b+"="+c);
+       //Pembagian
+       c=a/b;
+       System.out.println(a+":"+b+"="+c);
+       //Pembagian
+       c=a-b;
+       System.out.println(a+"-"+b+"="+c);
+       //Sisa Pembagian
+       c=a%b;
+       System.out.println(a+"%"+b+"="+c);
+       
+    }
+    
+}
